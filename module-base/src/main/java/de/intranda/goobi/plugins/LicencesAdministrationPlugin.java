4534cf87236e1b3e0fdf375e0495d661d623306d
Steffen Hankiewicz <steffen.hankiewicz@intranda.com>
2025-02-02 20:12:02 +0100

    small adaption of url

--- module-base/src/main/java/de/intranda/goobi/plugins/LicencesAdministrationPlugin.java
+++ module-base/src/main/java/de/intranda/goobi/plugins/LicencesAdministrationPlugin.java
@@ -31,7 +31,7 @@ public class LicencesAdministrationPlugin implements IAdministrationPlugin {
     @Getter
     private String title = "intranda_administration_licences";
     private List<Contract> contracts;
-    
+
     @Override
     public PluginType getType() {
         return PluginType.Administration;
@@ -42,69 +42,70 @@ public class LicencesAdministrationPlugin implements IAdministrationPlugin {
         return "/uii/plugin_administration_licences.xhtml";
     }
 
-    public List<Contract> getContracts(){
-    	if (contracts == null) {
-    		loadContracts();
-    	}
-    	return contracts;
+    public List<Contract> getContracts() {
+        if (contracts == null) {
+            loadContracts();
+        }
+        return contracts;
     }
-    
+
     /**
-	 * get all contracts via REST
-	 * @throws ClientProtocolException
-	 * @throws IOException
-	 */
-	public void loadContracts() {
-		String url = ConfigPlugins.getPluginConfig(title).getString("url");
+     * get all contracts via REST
+     *
+     * @throws ClientProtocolException
+     * @throws IOException
+     */
+    public void loadContracts() {
+        String url = ConfigPlugins.getPluginConfig(title).getString("url");
         String login = ConfigPlugins.getPluginConfig(title).getString("login");
         String password = ConfigPlugins.getPluginConfig(title).getString("password");
         String auth = Base64.getEncoder().encodeToString((login + ":" + password).getBytes());
-		
-		// send a GET request to localhost
-		try {
-			HttpResponse response;
-			response = Request.Get(url + "/contracts/")
-					.setHeader(HttpHeaders.AUTHORIZATION, "Basic " + auth)
-					.addHeader("accept", "application/json")
-					.useExpectContinue()
-				    .execute().returnResponse();
-		
-			// if response code is 200 analyse the result
-			if (response.getStatusLine().getStatusCode() == 200) {
-				contracts = new ArrayList<Contract>();
-				
-				// get result as string and convert it into JSON object
-				String responseAsString = EntityUtils.toString(response.getEntity());
-				ObjectMapper objectMapper = new ObjectMapper();
-				JsonNode jsonNode = objectMapper.readTree(responseAsString);
-				
-				// run through all contract nodes
-				ArrayNode arrayNode = (ArrayNode) jsonNode;
-				for (int i = 0; i < arrayNode.size(); i++) {
-					JsonNode node = arrayNode.get(i);
-					
-					Contract c = new Contract();
-					c.setName(node.get("name").asText());
-					c.setType(node.get("type").asText());
-					c.setOrdernumber(node.get("ordernumber").asText());
-					c.setEnabled(node.get("enabled").asBoolean());
-					c.setSize(node.get("size").asLong());
-					c.setUsedSize(node.get("usedSize").asLong());
-					c.setUsedPercent(node.get("usedPercent").asLong());
-					contracts.add(c);
-				}
-			}
-		} catch (IOException e) {
-			Helper.setFehlerMeldung("Error while contacting the licence server", e);
-			log.error("Error while contacting the licence server", e);
-		}
-	}
-    
-    
+
+        // send a GET request to localhost
+        try {
+            HttpResponse response;
+            response = Request.Get(url + "/contracts")
+                    .setHeader(HttpHeaders.AUTHORIZATION, "Basic " + auth)
+                    .addHeader("accept", "application/json")
+                    .useExpectContinue()
+                    .execute()
+                    .returnResponse();
+
+            // if response code is 200 analyse the result
+            if (response.getStatusLine().getStatusCode() == 200) {
+                contracts = new ArrayList<Contract>();
+
+                // get result as string and convert it into JSON object
+                String responseAsString = EntityUtils.toString(response.getEntity());
+                ObjectMapper objectMapper = new ObjectMapper();
+                JsonNode jsonNode = objectMapper.readTree(responseAsString);
+
+                // run through all contract nodes
+                ArrayNode arrayNode = (ArrayNode) jsonNode;
+                for (int i = 0; i < arrayNode.size(); i++) {
+                    JsonNode node = arrayNode.get(i);
+
+                    Contract c = new Contract();
+                    c.setName(node.get("name").asText());
+                    c.setType(node.get("type").asText());
+                    c.setOrdernumber(node.get("ordernumber").asText());
+                    c.setEnabled(node.get("enabled").asBoolean());
+                    c.setSize(node.get("size").asLong());
+                    c.setUsedSize(node.get("usedSize").asLong());
+                    c.setUsedPercent(node.get("usedPercent").asLong());
+                    contracts.add(c);
+                }
+            }
+        } catch (IOException e) {
+            Helper.setFehlerMeldung("Error while contacting the licence server", e);
+            log.error("Error while contacting the licence server", e);
+        }
+    }
+
     /**
      * Constructor
      */
     public LicencesAdministrationPlugin() {
         log.debug("Licence admnistration plugin started");
-    }   
+    }
 }
